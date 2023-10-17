aa867ea0c8a31ef910f6e6d1ed6f1f08fd1d830e
NicoleMuskett93 <nicolemuskett9@gmail.com>
2023-10-17 16:54:39 +0100

    section 10

--- functions.php
+++ functions.php
@@ -1,4 +1,35 @@
 <?php
+
+function pageBanner($args = NULL) {
+  
+    if (!isset($args['title'])) {
+      $args['title'] = get_the_title();
+    }
+   
+    if (!isset($args['subtitle'])) {
+      $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+   
+    if (!isset($args['photo'])) {
+      if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
+        $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+      } else {
+        $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+      }
+    }
+    ?>
+
+    
+<div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle']?></p>
+            </div>
+        </div>
+    </div>
+<?php }
 function university_files()
 {
     // wp_enqueue_style('university_main_styles', get_stylesheet_uri());
@@ -17,15 +48,7 @@ function university_files()
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features()
-{
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocationOne', 'Footer Location One');
-    register_nav_menu('footerLocationTwo', 'Footer Location Two');
-    add_theme_support('title-tag');
-}
 
-add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query){
  if(!is_admin() AND is_post_type_archive('program') AND is_main_query()){
@@ -53,3 +76,17 @@ function university_adjust_queries($query){
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function university_features()
+{
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner',1500,350, true);
+}
+
+add_action('after_setup_theme', 'university_features');
